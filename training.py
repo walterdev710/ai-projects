c508dcba7eeb98a371fd5c06014f29be4939e816
walterdev710 <abdullabek710@gmail.com>
2024-12-14 14:59:26 +0500

    Bug fixed

--- training.py
+++ training.py
@@ -67,7 +67,7 @@ model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
 
 
-sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+sgd = SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
 hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
